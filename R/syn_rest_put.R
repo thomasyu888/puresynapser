aab8cafebb25176f630edb201c70abf379ed4226
thomasyu888 <thomas.yu@sagebase.org>
2022-01-22 19:19:39 +0800

    Add rest PUT

--- R/syn_rest_put.R
+++ R/syn_rest_put.R
@@ -2,28 +2,26 @@
 ua <- httr::user_agent("https://github.com/thomasyu888/puresynapser")
 synapse_pat = Sys.getenv("SYNAPSE_AUTH_TOKEN")
 
-#' Synapse rest POST command
+#' Synapse rest PUT command
 #'
 #' @param path Path to synapse endpoint
 #' @param body Request Body
 #'
 #' @return Synapse API class
-#' @importFrom httr modify_url POST http_type status_code
+#' @importFrom httr modify_url PUT http_type status_code
 #' @importFrom jsonlite fromJSON
 #' @export
 #'
 #' @examples
-#' rest_POST("entity", body=list(name = "test-me-now",
-#'                               parentId="syn4990358",
-#'                               concreteType="org.sagebionetworks.repo.model.Folder"))
-rest_POST <- function(path, body) {
+#' rest_PUT("entity/syn26896636", body=list(name = "change-my-name"))
+rest_PUT <- function(path, body) {
   url <- httr::modify_url("https://repo-prod.prod.sagebase.org/repo/v1",
                           hostname="repo-prod.prod.sagebase.org/repo/v1",
                           path = path)
 
-  resp <- httr::POST(url, ua, body = body,
-                     httr::add_headers(Authorization = paste0("Bearer ", synapse_pat)),
-                     encode = "json")
+  resp <- httr::PUT(url, ua, body = body,
+                    httr::add_headers(Authorization = paste0("Bearer ", synapse_pat)),
+                    encode = "json")
   if (httr::http_type(resp) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
