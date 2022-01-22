14c56061893e120142caa9d04060471bbdd1650d
thomasyu888 <thomas.yu@sagebase.org>
2022-01-22 19:00:37 +0800

    ADd rest post

---
+++ R/syn_rest_post.R
@@ -0,0 +1,53 @@
+# set a user agent
+ua <- httr::user_agent("https://github.com/thomasyu888/puresynapser")
+synapse_pat = Sys.getenv("SYNAPSE_AUTH_TOKEN")
+
+#' Synapse rest POST command
+#'
+#' @param path Path to synapse endpoint
+#' @param body Request Body
+#'
+#' @return S3 object that is the same as the concreteType returned from Synapse.
+#' @importFrom httr modify_url GET http_type status_code
+#' @importFrom jsonlite fromJSON
+#' @export
+#'
+#' @examples
+#' rest_POST("entity", body=list(name = "test-me-now",
+#'                               parentId="syn4990358",
+#'                               concreteType="org.sagebionetworks.repo.model.Folder"))
+rest_POST <- function(path, body) {
+  url <- httr::modify_url("https://repo-prod.prod.sagebase.org/repo/v1",
+                          hostname="repo-prod.prod.sagebase.org/repo/v1",
+                          path = path)
+
+  resp <- httr::POST(url, ua, body = body(),
+                     httr::add_headers(Authorization = paste0("Bearer ", synapse_pat)))
+  if (httr::http_type(resp) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  parsed <- jsonlite::fromJSON(httr::content(resp, "text"), simplifyVector = FALSE)
+
+  if (httr::status_code(resp) != 200) {
+    stop(
+      sprintf(
+        "Synapse rest GET request failed [%s]\n%s\n<%s>",
+        httr::status_code(resp),
+        parsed$message,
+        parsed$documentation_url
+      ),
+      call. = FALSE
+    )
+  }
+
+  structure(
+    list(
+      content = parsed,
+      path = path,
+      response = resp
+    ),
+    class = gsub("org.sagebionetworks.repo.model.", "", parsed$concreteType)
+
+  )
+}
