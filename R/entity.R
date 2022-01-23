900545839227ffd7f9ca338b8f1cc8b0fd59f480
thomasyu888 <thomas.yu@sagebase.org>
2022-01-23 20:05:34 +0800

    Add function to get entity

--- R/entity.R
+++ R/entity.R
@@ -1,4 +1,22 @@
+#' Entity object
+#'
+#' @param name Name of entity
+#' @param description Description of entity
+#' @param id Synapse Id of entity
+#' @param etag Etag of entity
+#' @param parentId Synapse Id of the container the entity is contained in.
+#' @param createdOn Created on date
+#' @param modifiedOn Modified on date
+#' @param createdBy Created by date
+#' @param modifiedBy modified by date
+#' @param concreteType Concrete Type
+#'
+#' @return Entity Class
+#'
+#' @examples
+#' new_entity("new_ent", name="entity_name", id="syn...")
 new_entity <- function(
+  x,
   name = character(),
   description = "",
   id = "",
@@ -14,7 +32,8 @@ new_entity <- function(
   # units <- match.arg(units, c("secs", "mins", "hours", "days", "weeks"))
 
   structure(
-    name,
+    x,
+    name = name,
     description = description,
     id = id,
     etag = etag,
@@ -28,47 +47,40 @@ new_entity <- function(
   )
 }
 
-validate_entity <- function(x) {
-  values <- unclass(x)
-  x
-}
+# validate_entity <- function(x) {
+#   values <- unclass(x)
+#   x
+# }
 
-#' Folder Entity
+get_syn_id <- function(x) attr(x, "id")
+
+#' Get entity
 #'
-#' @param name
-#' @param description
-#' @param id
-#' @param etag
-#' @param parentId
+#' @param id_or_ent Synapse Id or Entity object
 #'
-#' @return
+#' @return Entity
 #' @export
 #'
 #' @examples
-folder <- function(
-  name = "",
-  description = "",
-  id = "",
-  etag = "",
-  createdOn = "",
-  modifiedOn = "",
-  createdBy = "",
-  modifiedBy = "",
-  parentId = "",
-  concreteType = ""
-) {
-  validate_entity(new_entity(
-    name = name,
-    description = description,
-    id = id,
-    etag = etag,
-    createdOn = createdOn,
-    modifiedOn = modifiedOn,
-    createdBy = createdBy,
-    modifiedBy = modifiedBy,
-    parentId = parentId,
-    concreteType = syn_global$concrete_types$FOLDER_ENTITY
-  ))
+#' get_entity("syn...")
+get_entity <- function(id_or_ent) {
+  if (inherits(id_or_ent, "Folder")) {
+    syn_id = get_syn_id(id_or_ent)
+  } else {
+    syn_id = id_or_ent
+  }
+  ent_resp = rest_GET(sprintf("entity/%s", syn_id))
+  ent_info = ent_resp$content
+  new_entity(
+    ent_info$name,
+    name = ent_info$name,
+    id = ent_info$id,
+    etag = ent_info$etag,
+    createdOn = ent_info$createdOn,
+    modifiedOn = ent_info$modifiedOn,
+    createdBy = ent_info$createdBy,
+    modifiedBy = ent_info$modifiedBy,
+    parentId = ent_info$parentId,
+    concreteType = ent_info$concreteType
+  )
 }
-
-errors <- function(x) attr(x, "errors")
